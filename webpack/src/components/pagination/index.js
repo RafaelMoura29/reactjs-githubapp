39382e24492f8f64026c3b6158a0cd6893392df4
Rafael Moura <rafaelmoura2929@gmail.com>
2020-04-23 16:36:07 -0300

    Criando storybook da pagination

---
+++ webpack/src/components/pagination/index.js
@@ -0,0 +1,20 @@
+'use strict'
+
+import React from 'react'
+import pagination from '../../utils/pagination/index'
+
+const Pagination = ({ total, activePage }) => (
+    <ul>
+
+        {pagination({ total, activePage }).map((page, index) => (
+            <li key={index}>
+                <a href="#">
+                    {page}
+                </a>
+            </li>
+        )
+        )}
+    </ul>
+)
+
+export default Pagination
---
+++ webpack/src/components/pagination/pagination.story.js
@@ -0,0 +1,13 @@
+'use strict'
+
+import React from 'react'
+import { storiesOf } from '@kadira/storybook'
+import Pagination from './index'
+
+const stories = storiesOf('<Pagination />', module)
+
+stories.add("without props", () => (
+    <Pagination 
+    total={10}
+    />
+))
