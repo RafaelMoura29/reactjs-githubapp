403f93fa33cd0ab65871c1d5027b5dbf243d9474
Rafael Moura <rafaelmoura2929@gmail.com>
2020-04-25 12:31:22 -0300

    Fazendo callback do pagination

---
+++ webpack/src/components/pagination/page.js
@@ -0,0 +1,19 @@
+'use strict'
+
+import React from 'react'
+
+const Page = ({ page, pageLink, onClick }) => {
+  const Component = page === "..." ? "span" : "a"
+  const href = page === "..." ? null : pageLink
+  const handleClick = !onClick ? null : (e) => {
+    e.preventDefault()
+    onClick(page)
+  }
+  return (
+    <Component href={href} onClick={handleClick}>
+      {page}
+    </Component>
+  )
+}
+
+export default Page
--- webpack/src/components/pagination/pagination.story.js
+++ webpack/src/components/pagination/pagination.story.js
@@ -7,7 +7,33 @@ import Pagination from './index'
 const stories = storiesOf('<Pagination />', module)
 
 stories.add("without props", () => (
-    <Pagination 
+  <Pagination/>
+))
+
+stories.add("with total and active page", () => (
+  <Pagination
     total={10}
-    />
+    activePage={5}
+  />
+))
+
+stories.add("with pagelink", () => (
+  <Pagination
+    total={3}
+    activePage={1}
+    pageLink="http://mypage.com/page/%page%"
+  />
+))
+
+stories.add("with callback", () => (
+  <Pagination
+    total={15}
+    activePage={7}
+    pageLink="http://mypage.com/page/%page%"
+    onClick={(page) => {
+      return (
+        window.alert(page)
+      )
+    }}
+  />
 ))
